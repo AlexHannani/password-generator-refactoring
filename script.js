3dc1f5dda2418e7f3f4378e2fa263917f7b53392
Alex Hannani <alexhannani24@gmail.com>
2022-12-07 01:42:58 -0500

    added main generate password function with char length alert

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ var arLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 var arUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var arNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var arSpecial = ["!", "@", "#", "$", "%", "&", "*", "?", "_", "-", "^"];
+
+function generatePassword() {
+  var characterLength = prompt("How many characters do you want your password to be? Please select a number between 8 and 128")
+  if (characterLength < 8 || characterLength > 128) {
+    alert("Please enter a number between 8 and 128");
+    generatePassword();
+  }
+
+  
+}
